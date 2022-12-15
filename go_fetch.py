37998413a1424f72e58178156158c3c1a03b7b28
RobertAlden <randydonst@gmail.com>
2022-12-14 21:17:49 -0600

    Automated templating

--- go_fetch.py
+++ go_fetch.py
@@ -1,7 +1,6 @@
+import os
 import requests
 import argparse
-from os.path import exists
-
 
 parser = argparse.ArgumentParser(description='Gets the Advent of Code input for the given day\'s problem.')
 parser.add_argument('query_date', metavar='d', type=str, help='Format: YYYY-DD')
@@ -14,16 +13,22 @@ year, day = tuple(args.query_date.split('-'))
 
 file_location = f'./{year}/day{day}/input.txt'
 
-if not (file_exists := exists(file_location)):
+if not (file_exists := os.path.exists(file_location)):
     print("Input not found, attempting to retrieve input.")
     target_url = f'https://adventofcode.com/{year}/day/{day}/input'
     print(target_url)
     r = requests.get(target_url, cookies={'session': my_session})
     match r.status_code:
         case 200:
+            if not os.path.exists(file_location[:-9]):
+                os.makedirs(file_location[:-9])
             with open(file_location, 'w') as f:
                 f.write(r.text)
             print(f'Retrieval succeeded.')
+            cmd = f'copy \"{os.getcwd()}\\{year}\\_new\\rename_me.py\" \"{os.getcwd()}\\{year}\\day{day}\\day{day}.py\"'
+            with open(file_location[:-9] + 'test.txt', 'w') as f:
+                f.write("GO FIND THE TEST INPUT LOSER")
+            os.system(cmd)
         case _ as status_code:
             print(f'Retrieval failed: Status Code:{status_code}')
 else:
