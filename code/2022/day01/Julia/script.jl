b1b4a6a9f3196e4359cf8828a0f8f94abf1b8eed
RobertAlden <randydonst@gmail.com>
2023-06-07 08:43:55 -0500

    changes duh

--- code/2022/day01/Julia/script.jl
+++ code/2022/day01/Julia/script.jl
@@ -1,31 +1,13 @@
-MODE = "test"
-
 test = readlines("data/2022/day01/test.txt")
 data = readlines("data/2022/day01/input-[HIDE]-.txt")
 
-function listSplit(data, sep)
-    bool_seq = [(i == sep) for i in data]
-    seq = Vector{Vector{Any}}()
-    chunk = Vector{Any}()
-    for (b, v) in zip(bool_seq, data)
-        if !b
-            push!(chunk, v)
-        else
-            push!(seq, copy(chunk))
-            empty!(chunk)
-        end
-    end
-    push!(seq, chunk)
-    seq
-end
-
-function listSplit2(data, sep)
+@views function listSplit(data, sep)
     indices = findall(x -> x == sep, data)
     if length(indices) == 0
         return [data]
     end
     indices = [0; indices; length(data)]
-    return [data[indices[i]+1:indices[i+1]-1] for i in 1:length(indices)-1]
+    [data[indices[i]+1:indices[i+1]-1] for i in 1:length(indices)-1]
 end
 
 function silver(data::Array{String,1})
@@ -39,12 +21,6 @@ function gold(data::Array{String,1})
     map(inner, chunks) |> sort |> x -> last(x, 3) |> sum
 end
 
-# @time @show silver(test)
-# @time @show gold(test)
+@time println(silver(data))
 
-# @time @show silver(data)
-# @time @show gold(data)
-
-@btime listSplit(data, "\n")
-
-@btime listSplit2(data, "\n")
+@time println(gold(data))
