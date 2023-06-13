3032f18e8400c9ad579dac75162a11c46d098b68
RobertAlden <randydonst@gmail.com>
2023-06-13 14:45:38 -0500

    wip

--- code/2022/day07/Julia/script.jl
+++ code/2022/day07/Julia/script.jl
@@ -1,12 +1,20 @@
+include("../../../../lib/Julia/AoC_Utils.jl")
+using .AoC_Utils: chunk_items
 
+function parse(data)
+    dirs = findall(x -> contains(x, " cd "), data)
+    dirs = filter(x -> !contains(x, ".."), data[dirs])
+    dirs = @. collect(last(split(dirs, " cd ")))
 
-struct dir
-    size::Int
-    name::String
-    contents::Array{dir,1}
+    contents = chunk_items(data, x -> startswith(x, "\$"))
+    cleanup(str) = startswith(str, "dir") ? last(split(str, " ")) : first(split(str, " "))
+    contents = map(x -> cleanup.(x), contents)
+
+    zip(dirs, contents)
 end
 
 function silver(data)
+    dir_tree = Dict(parse(data))
 
 end
 
@@ -16,13 +24,13 @@ end
 
 
 test = readlines("data/2022/day07/test.txt")
-input = readlines("data/2022/day07/input.txt")
+data = readlines("data/2022/day07/input.txt")
 
 @time println(silver(test))
 @time println(gold(test))
 
-@time println("Silver: $(silver(data))")
-@time println("Gold: $(gold(data))")
+# @time println("Silver: $(silver(data))")
+# @time println("Gold: $(gold(data))")
 
-@btime silver(data)
-@btime gold(data)
+# @btime silver(data)
+# @btime gold(data)
--- lib/Julia/AoC_Utils.jl
+++ lib/Julia/AoC_Utils.jl
@@ -5,6 +5,7 @@ export windowed, parse_into_struct, chunk_items
 windowed(arr, n) = @views [arr[i:(i+n-1)] for i in firstindex(arr):lastindex(arr)-n+1]
 
 function parse_into_struct(str::String, datatype, seps::String=nothing)
+    ### Parse a string into a struct, using the field types as a guide
     if isnothing(seps)
         seps = " "
     end
@@ -26,13 +27,15 @@ function parse_into_struct(str::String, datatype, seps::String=nothing)
     datatype(values...)
 end
 
-function chunk_items(data, sep)
-    indices = findall(x -> x == sep, data)
+function chunk_items(data, f, dropempty=true)
+    ### Divide a collection into chunks based on a boolean function
+    indices = findall(f, data)
     if length(indices) == 0
         return [data]
     end
     indices = [firstindex(data) - 1; indices; lastindex(data) + 1]
-    [data[indices[i]+1:indices[i+1]-1] for i in firstindex(indices):lastindex(indices)-1]
+    results = [data[indices[i]+1:indices[i+1]-1] for i in firstindex(indices):lastindex(indices)-1]
+    dropempty ? collect(filter(x -> !isempty(x), results)) : collect(results)
 end
 
 end
