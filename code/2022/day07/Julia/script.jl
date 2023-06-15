1a0a00d67d2f6729c56f1cd41716d522c87ee650
RobertAlden <randydonst@gmail.com>
2023-06-14 22:28:10 -0500

    wip

--- code/2022/day07/Julia/script.jl
+++ code/2022/day07/Julia/script.jl
@@ -4,18 +4,35 @@ using .AoC_Utils: chunk_items
 function parse(data)
     dirs = findall(x -> contains(x, " cd "), data)
     dirs = filter(x -> !contains(x, ".."), data[dirs])
-    dirs = @. collect(last(split(dirs, " cd ")))
+    dirs = last.(split.(dirs, " cd "))
 
     contents = chunk_items(data, x -> startswith(x, "\$"))
+
     cleanup(str) = startswith(str, "dir") ? last(split(str, " ")) : first(split(str, " "))
     contents = map(x -> cleanup.(x), contents)
 
     zip(dirs, contents)
 end
 
+function splitmap(f, coll)
+    somethings = f.(filter(x -> !isnothing(f(x)), coll))
+    nothings = filter(x -> isnothing(f(x)), coll)
+    somethings, nothings
+end
+
+function compute_node(tree, node)
+    dir_contents, sub_dirs = splitmap(x -> tryparse(Int, x), tree[node])
+    isempty(sub_dirs) && return sum(dir_contents)
+    dir_size = isempty(dir_contents) ? 0 : sum(dir_contents)
+    dir_size + sum(map(x -> compute_node(tree, x), sub_dirs))
+end
+
 function silver(data)
     dir_tree = Dict(parse(data))
-
+    println(collect(keys(dir_tree)))
+    dir_sizes = map(x -> compute_node(dir_tree, x), collect(keys(dir_tree)))
+    println(dir_sizes)
+    sum(filter(x -> x <= 100000, dir_sizes))
 end
 
 function gold(data)
@@ -23,13 +40,13 @@ function gold(data)
 end
 
 
-test = readlines("data/2022/day07/test.txt")
-data = readlines("data/2022/day07/input.txt")
+const test = readlines("data/2022/day07/test.txt")
+const data = readlines("data/2022/day07/input.txt")
 
 @time println(silver(test))
 @time println(gold(test))
 
-# @time println("Silver: $(silver(data))")
+@time println("Silver: $(silver(data))")
 # @time println("Gold: $(gold(data))")
 
 # @btime silver(data)
